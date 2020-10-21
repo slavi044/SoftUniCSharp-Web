773be5be3cda6a449e7c2b75f3c4981104465fa9
slavi044 <slavi044@abv.bg>
2020-10-21 13:44:38 +0300

    Create DataBase

--- C#WebBasics/ExamPreparation/C#WebDevelopmentBasicsExam-16Feb2020/SharedTrip/Data/ApplicationDbContext.cs
+++ C#WebBasics/ExamPreparation/C#WebDevelopmentBasicsExam-16Feb2020/SharedTrip/Data/ApplicationDbContext.cs
@@ -1,18 +1,46 @@
 ﻿namespace SharedTrip
 {
     using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Internal;
+    using SharedTrip.Models;
 
     public class ApplicationDbContext : DbContext
-    { 
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+
+        public ApplicationDbContext(DbContextOptions db)
+            :base(db)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Trip> Trips { get; set; }
+
+        public DbSet<UserTrip> UserTrips { get; set; }
+
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            base.OnConfiguring(optionsBuilder);
-
-            optionsBuilder.UseSqlServer(DatabaseConfiguration.ConnectionString);
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=SharedTrip;Integrated Security=True");
+            }
+            else
+            {
+                base.OnConfiguring(optionsBuilder);
+            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<UserTrip>()
+                .HasKey(x => new { x.UserId, x.TripId });
+
             base.OnModelCreating(modelBuilder);
         }
     }
