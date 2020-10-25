103677d6d1b26b9174253b8f91fd230af010de97
slavi044 <slavi044@abv.bg>
2020-10-25 09:29:44 +0200

    Initialize database

---
+++ C#WebBasics/C#WebDevelopmentBasics-25October2020/Skeleton/Apps/Git/Data/Commit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Git.Data
+{
+    public class Commit
+    {
+        public Commit()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+        public string CreatorId { get; set; }
+        public User Creator { get; set; }
+
+        public string RepositoryId { get; set; }
+        public Repository Repository { get; set; }
+    }
+}
---
+++ C#WebBasics/C#WebDevelopmentBasics-25October2020/Skeleton/Apps/Git/Data/Repository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Git.Data
+{
+    public class Repository
+    {
+        public Repository()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.Commits = new HashSet<Commit>();
+        }
+
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+        [Required]
+        public bool IsPublic { get; set; }
+
+        public string OwnerId { get; set; }
+        public User Owner { get; set; }
+
+        public virtual ICollection<Commit> Commits { get; set; }
+    }
+}
